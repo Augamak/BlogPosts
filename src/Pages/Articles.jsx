3cc22fc45cbe6ccf1ec85c7dd5c9d9e5816b4735
Augamak <augusciukass93@gmail.com>
2024-02-11 22:25:02 +0200

    add  1-10

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -4,27 +4,49 @@ import { Link } from "react-router-dom";
 const Articles = () => {
 
     const [articles, setArticles] = useState([])
-    console.log(articles.blogs)
+    console.log(articles)
+    const [number, setNumber] = useState(0);
+
+    const right = () => {
+        setNumber((prev) => {
+          if (prev === 1000) return 10;
+          return prev +10
+        });
+      }
+    
+      const left = () => {
+        setNumber((prev) => {
+          if (prev === 1000) return 10;
+          return prev - 10
+        })
+      }
 
     useEffect(() => {
-        fetch("https://api.slingacademy.com/v1/sample-data/blog-posts")
+        fetch(`https://api.slingacademy.com/v1/sample-data/blog-posts?offset=${number}&limit=${10}`)
         .then((response) => response.json())
         .then((blogs) => setArticles(blogs.blogs))
+        
        
-    }, []);
+    }, [number]);
 
     return (
         <div className="about">
-            {articles.map((art) => {
-            return (
-                <div className="cards" key={art.id}>
-                    <Link className="article-link" to={`/articles/${art.id}`}>
-                        {art.id} - {art.title}
-                    </Link>
-                   
-                </div>
-            );
-            })}
+            <div className="arrow">
+                <span onClick={left} className='simbol-arrow'>⇐</span>
+                <span onClick={right} className='simbol-arrow'>⇒</span>{" "}
+            </div>
+            <div className="cards-blog">
+                {articles.map((art) => {
+                return (
+                    <div className="cards" key={art.id}>
+                        <Link className="article-link" to={`/articles/${art.id}`}>
+                            {art.id} - {art.title}
+                        </Link>
+                    </div>
+                );
+                })}
+            </div>
+            
         </div> 
     );
 };
